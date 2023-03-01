175359d60b6a8a125289550abb5875975a2f0787
Navneet Bahuguna <navneetbahuguna007@gmail.com>
2023-03-01 14:52:43 +0530

    cors policy

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express');
 const createError = require('http-errors');
 const morgan = require('morgan');
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
+
+app.use(cors());
 
 app.get('/', async (req, res, next) => {
   res.send({ message: 'Awesome it works 🐻' });
